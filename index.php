1c384d281c36ebbfd38a9578c8bd24d7d9b155a9
Kianna Dyck <kdyck@mail.greenriver.edu>
2018-01-19 15:59:02 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Kianna Dyck
+ * Jen Shin
+ */
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an instance of the Base class
+$f3 = Base::instance();
+
+// Set debug level 0 = off, 3 = max level
+$f3->set('DEBUG', 3);
+
+// Define a default route
+$f3->route('GET /', function() {
+    $template = new Template();
+    echo $template->render('views/home.html');
+});
+
+// Run fat free
+$f3->run();
