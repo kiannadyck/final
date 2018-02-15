a625866ab6fa554b4ab2e6ea3b88ae7d99d334fe
Kianna Dyck <kdyck@mail.greenriver.edu>
2018-02-15 16:39:42 -0500

    Created models and classes directories. Created some mostly empty files inside views directory and tested/created some routes for these pages inside index file. Added name for selecting a deck dropdown so it submits to post array. Modified default route so form can post to itself.

--- index.php
+++ index.php
@@ -11,6 +11,9 @@ error_reporting(E_ALL);
 // Require the autoload file
 require_once('vendor/autoload.php');
 
+// Start a session
+session_start();
+
 // Create an instance of the Base class
 $f3 = Base::instance();
 
@@ -18,15 +21,82 @@ $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
 // Define a default route
-$f3->route('GET /', function($f3) {
+$f3->route('GET|POST /', function($f3) {
+
+    // Define array of decks (stored in database)
     $f3->set('options', array(
             'deck1',
             'deck2',
             'deck3')
     );
 
+    //    print_r($_POST);
+
+    /*Array (
+        [deckOption] => deck2
+        [choice] => play
+        [submit] => Submit )*/
+
+    if(isset($_POST['submit']))
+    {
+        // retrieve values from POST array
+        $deck = $_POST['deckOption'];
+        $choice = $_POST['choice'];
+
+        // validate data
+
+        // if data is valid, retrieve deck from database
+
+        // store deck in session?
+
+        // Reroute to next page depending on choice
+        if ($choice == "play") {
+            $f3->reroute('/play');
+        } else if ($choice == "edit") {
+            $f3->reroute('/edit');
+        }
+
+    }
+
+
+
     $template = new Template();
     echo $template->render('views/home.html');
+});
+
+// Define a route for playing/viewing flashcards
+$f3->route('GET /play', function($f3) {
+
+    $template = new Template();
+    echo $template->render('views/view-flashcards.html');
+});
+
+// Define a route for editing flashcards from an existing deck
+$f3->route('GET /edit', function($f3) {
+
+    $template = new Template();
+    echo $template->render('views/edit-flashcards.html');
+});
+
+// Define a route for creating a new deck
+$f3->route('GET /create', function($f3) {
+
+    $template = new Template();
+    echo $template->render('views/create-new-deck.html');
+});
+
+// Define route for login
+$f3->route('GET /login', function($f3) {
+
+    $template = new Template();
+    echo $template->render('views/login.html');
+});
+
+// Define route for registration of new users
+$f3->route('GET /register', function($f3) {
+
+    $template = new Template();
+    echo $template->render('views/register.html');
 });
 
 // Run fat free
---
+++ models/db-functions.php
@@ -0,0 +1,19 @@
+<?php
+// require database connection file
+//(probably need to change path)
+require ("/home/kdyckgre/config.php");
+
+function connect()
+{
+    try {
+        // instantiate a PDO object using a PDO constructor
+        $dbh = new PDO(DB_DSN,
+            DB_USERNAME,
+            DB_PASSWORD);
+//        echo "Connected to database!";
+        return $dbh;
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        return;
+    }
+}
