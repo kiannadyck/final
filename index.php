dc9e75ed18dae9c657ee8dfe157942d3b2573a08
Kianna Dyck <kdyck@mail.greenriver.edu>
2018-02-15 15:30:06 -0500

    Fixed issue with route in index page. Fixed issue with title in home page. Passes validation test.

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
  * Jen Shin
  */
 
+// Error Reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 // Require the autoload file
 require_once('vendor/autoload.php');
 
@@ -13,14 +17,14 @@ $f3 = Base::instance();
 // Set debug level 0 = off, 3 = max level
 $f3->set('DEBUG', 3);
 
-$f3->set('options', array(
-        'deck1',
-        'deck2',
-        'deck3')
-);
-
 // Define a default route
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
+    $f3->set('options', array(
+            'deck1',
+            'deck2',
+            'deck3')
+    );
+
     $template = new Template();
     echo $template->render('views/home.html');
 });
